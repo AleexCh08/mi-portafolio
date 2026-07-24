5ac7b3616c059c008137b91dd263e72d5da67f5c
Alexander <aleexch08@gmail.com>
2026-07-23 23:20:08 -0400

    mejorado hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export function Hero() {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="relative py-10"
+      className="relative"
     >
     <AuroraBackground />
       <div className="text-center relative z-10">
